162b86606209d07f35a5e46ca6a391de9d7690f1
Mateusz <kasprzak000@gmail.com>
2021-11-05 13:05:49 +0100

    is loaded house details

--- src/pages/HousePage/HousePage.jsx
+++ src/pages/HousePage/HousePage.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import './HousePage.css'
 
 const HousePage = () => {
+    const [isError, setIsError] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [houseDetails, setHouseDetails] = useState({});
     const { id } = useParams();
     
@@ -18,7 +20,6 @@ const HousePage = () => {
         .then(res => res.json())
         .then(
             (result) => {
-                console.log(result)
                 setHouseDetails({
                     name: result.name,
                     region: result.region !== '' ? result.region : 'Unknown',
@@ -30,47 +31,66 @@ const HousePage = () => {
                     hasOverloard: result.overloard !== '' ? 'Yes' : 'No',
                     numberOfCadetBranches: result.cadetBranches.length,
                 })
+                setIsLoaded(true);
+                setIsError(false);
             },
             (error) => {
-                console.warn(error)
+                setIsLoaded(true);
+                setIsError(true);
             }
         )
     },[id])
 
-    return (
-        <main className='house-page'>
-            <header>
-                <Link to='/'><span className="material-icons">arrow_back</span></Link>
-                <h1>{houseDetails.name}</h1>
-            </header>
-            <table className='house-table'>
-                <thead>
-                    <tr>
-                        <th>Coat of Arms</th>
-                        <th>Region</th>
-                        <th>Words</th>
-                        <th>Titles</th>
-                        <th>Seats</th>
-                        <th>Has died out</th>
-                        <th>Has overlord</th>
-                        <th>Number of Cadet Branches</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{houseDetails.coatOfArms}</td>
-                        <td>{houseDetails.region}</td>
-                        <td>{houseDetails.words}</td>
-                        <td>{houseDetails.titles}</td>
-                        <td>{houseDetails.seats}</td>
-                        <td>{houseDetails.hasDiedOut}</td>
-                        <td>{houseDetails.hasOverloard}</td>
-                        <td>{houseDetails.numberOfCadetBranches}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </main>
-    )
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    } else {
+        if (isError) {
+            return (
+            <div style={{textAlign:'center'}}>
+                <h1>404</h1>
+                <h2>Page not found</h2>
+                <Link to='/'>home page</Link>
+            </div>
+            )
+        } else {
+            return (
+                <main className='house-page'>
+                    <header>
+                        <Link to='/'><span className="material-icons">arrow_back</span></Link>
+                        <h1>{houseDetails.name}</h1>
+                    </header>
+                    <table className='house-table'>
+                        <thead>
+                            <tr>
+                                <th>Coat of Arms</th>
+                                <th>Region</th>
+                                <th>Words</th>
+                                <th>Titles</th>
+                                <th>Seats</th>
+                                <th>Has died out</th>
+                                <th>Has overlord</th>
+                                <th>Number of Cadet Branches</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{houseDetails.coatOfArms}</td>
+                                <td>{houseDetails.region}</td>
+                                <td>{houseDetails.words}</td>
+                                <td>{houseDetails.titles}</td>
+                                <td>{houseDetails.seats}</td>
+                                <td>{houseDetails.hasDiedOut}</td>
+                                <td>{houseDetails.hasOverloard}</td>
+                                <td>{houseDetails.numberOfCadetBranches}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </main>
+            )
+        }
+    }
+
+    
 }
 
 export default HousePage;
